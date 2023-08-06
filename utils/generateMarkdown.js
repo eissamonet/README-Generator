3dfb5986fd170adeade84e04f3968a5cb4d1542b
Eissa Purnell <eissamonet@gmail.com>
2023-08-06 11:35:05 -0500

    changes made to markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// a function to generate markdown for README
 const generateMarkdown = data => {
   return `# ${data.title}
   ![GitHub](https://img.shields.io/github/license-${data.license}-yellow)
@@ -30,7 +20,6 @@ const generateMarkdown = data => {
   ## Installation
   ${data.install}
 
-
   ## Usage
   ${data.usage}
 
@@ -44,15 +33,7 @@ const generateMarkdown = data => {
   ${data.tests}
 
   ## Questions
-  Feel free to email me at ${data.email} with questions. You can view more of my projects here:  
-
-
-
-
-
-
-
-
+  Feel free to email me at ${data.email} with questions. You can view more of my projects here:[${data.username}](https://github.com/${data.username}). 
 
 `;
 }
